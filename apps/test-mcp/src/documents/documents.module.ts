bd917ef0be0fbdb64b98e58a042fc89d0bbb209f
Francisco Galarza <franciscogalarza@gmail.com>
2025-11-10 21:34:14 +1100

    missing inport

--- apps/test-mcp/src/documents/documents.module.ts
+++ apps/test-mcp/src/documents/documents.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { DocumentsService } from './documents.service';
 import { DocumentsController } from './documents.controller';
 import { Documents } from './documents';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   providers: [DocumentsService, Documents],
   controllers: [DocumentsController]
 })
-export class DocumentsModule {}
+export class DocumentsModule { }
