5932d38a64b6b63c216ae34ca894f22e9b5d3fbf
Francisco Galarza <franciscogalarza@gmail.com>
2026-01-06 12:32:44 +1100

    better mobile ui (#315)

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -6,6 +6,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    allowedHosts: [
+      'air.local'
+    ],
     port: Number(process.env.VITE_PORT) || 5173,
     proxy: {
       "/api": {
