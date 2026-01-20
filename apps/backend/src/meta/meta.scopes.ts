1d15b6b14755d3ba97e10e4494766ce11c872ce5
Francisco Galarza <franciscogalarza@gmail.com>
2026-01-20 15:04:33 +1100

    add meta scopes to login (#377)

--- apps/backend/src/meta/meta.scopes.ts
+++ apps/backend/src/meta/meta.scopes.ts
@@ -1,3 +1,5 @@
+import { Scope } from "src/auth/core/types/scope.type";
+
 export const MetaScopes = {
   READ: {
     id: 'meta:read',
@@ -7,4 +9,7 @@ export const MetaScopes = {
     id: 'meta:write',
     description: 'Create, update, and delete meta information (tags, etc.)',
   },
-} as const;
+} as const satisfies Record<string, Scope>;
+
+export const ALL_META_SCOPES: readonly Scope[] =
+  Object.values(MetaScopes);
